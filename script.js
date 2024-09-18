a4d7dca8a87f709e50f5d2ef92e01022e45cd6ac
heloisagasparlima <00001138516971sp@al.educacao.sp.gov.br>
2024-09-18 09:26:23 -0300

    adc imagem

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const avanca=document.queryAelectorAll('.btn-proximo');
 
 avanca.forEach (button=>) {
-    button.addEventListener('.click',function(){
+    button.addEventListener('.click',function() {
         const atual= document.querySelector('.ativo');
         const proximoPasso= 'passo-'+ this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementByld(proximoPasso).classList.add('ativo');
-    }
+    })
 
 })
